2eef273932c64600d9008f82b1a970cc191e1c75
George <george32451@mail.ru>
2020-08-17 13:43:10 +0300

    feat: added routing; feat: added product card page; feat: created separate component for "add to card" button;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'products' },
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductCardComponent },
+    ]
+  },
+  { path: '**', redirectTo: '/products' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlus, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faShoppingCart, faUser, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductListItemComponent } from './product-list/product-list-item/product-list-item.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { AddToCardButtonComponent } from './shared/add-to-card-button/add-to-card-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProductListComponent,
-    ProductListItemComponent
+    ProductListItemComponent,
+    ProductCardComponent,
+    AddToCardButtonComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +33,6 @@ import { ProductListItemComponent } from './product-list/product-list-item/produ
 })
 export class AppModule {
   constructor(private library: FaIconLibrary) {
-    library.addIcons(faUser, faShoppingCart, faPlus);
+    library.addIcons(faUser, faShoppingCart, faPlus, faArrowLeft);
   }
 }
