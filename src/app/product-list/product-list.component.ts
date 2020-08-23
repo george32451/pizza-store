e1b66b8fe620555d4282a46e5d11a699596b3052
George <george32451@mail.ru>
2020-08-23 14:53:31 +0300

    feat: show spinner on product list page, when loading products, set minimum amount of time of loading products to 1 second;

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { select, Store } from '@ngrx/store';
 
 import { Product } from 'models/interfaces/product.interface';
 import * as fromApp from 'store/app.reducer';
 import * as ProductListActions from './store/product-list.actions';
+import * as ProductListSelectors from './store/product-list.selectors';
 
 @Component({
   selector: 'app-product-list',
@@ -15,15 +15,14 @@ import * as ProductListActions from './store/product-list.actions';
 })
 export class ProductListComponent implements OnInit {
   public products$: Observable<Product[]>;
+  public isLoading$: Observable<boolean>;
 
   constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
     this.store.dispatch(ProductListActions.getProductListStart());
-    this.products$ = this.store.pipe(
-      select('productList'),
-      map(productList => productList.products)
-    );
+    this.products$ = this.store.pipe(select(ProductListSelectors.getProducts));
+    this.isLoading$ = this.store.pipe(select(ProductListSelectors.isProductListLoading));
   }
 
   public trackById(index: number, product: Product): number {
